cdaaae69eb6574cf1bdbdcda59991d168272bf43
yhocn107 <yhocn107@163.com>
2019-05-23 17:03:16 +0800

    5.23

--- miniprogram/pages/frmmanual_inputDian/frmmanual_inputDian.js
+++ miniprogram/pages/frmmanual_inputDian/frmmanual_inputDian.js
@@ -1,18 +1,45 @@
 // miniprogram/pages/frmmanual_inputDian/frmmanual_inputDian.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    hiddenName8: true,
+  
+    idd8: 0,
+    
+    id8: 0,
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    app.editTabBar1();
+  },
+  clickMe8: function (e) {
+    var idd8 = e.target.id
+    console.log(idd8)
+    this.setData({
+      hiddenName8: !this.data.hiddenName8
+    })
 
+  },
+  click8: function (e) {
+    var id8 = e.target.id
+    var idd8 = e.target.dataset.id
+    console.log(id8)
+    console.log(idd8)
+    this.setData({
+      hiddenName8: !this.data.hiddenName8,
+      id8: id8,
+      idd8: idd8,
+   
+    })
+    
   },
 
   /**
